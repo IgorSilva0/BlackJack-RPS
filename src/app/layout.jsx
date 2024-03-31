ca85d2e4f55e07877841d2ad3e7f5dc394a879a7
Igor Silva <igorsilvauk@gmail.com>
2024-03-31 12:04:21 +0100

    react icons added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { FaApple } from "react-icons/fa6";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <FaApple size="50px"/>
+        {children}
+        <FaApple/>
+      </body>
     </html>
   );
 }
